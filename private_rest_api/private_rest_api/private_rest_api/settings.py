a4e0e5fe413be8ae49966e0efe543ceb3f4cc316
MatthewTee <teelucksingh.matthew1@gmail.com>
2022-02-01 22:12:15 -0500

    .

--- private_rest_api/private_rest_api/private_rest_api/settings.py
+++ private_rest_api/private_rest_api/private_rest_api/settings.py
@@ -89,10 +89,10 @@ if DEBUG:
     }
 else:
     REST_FRAMEWORK = {        
-        #"DEFAULT_AUTHENTICATION_CLASSES": [
-        #    "rest_framework.authentication.BasicAuthentication"
-        #    "rest_framework.authentication.SessionAuthentication",
-        #    "rest_framework.authentication.TokenAuthentication"],
+        "DEFAULT_AUTHENTICATION_CLASSES": [
+            "rest_framework.authentication.BasicAuthentication"
+            "rest_framework.authentication.SessionAuthentication",
+            "rest_framework.authentication.TokenAuthentication"],
 
         "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
 
