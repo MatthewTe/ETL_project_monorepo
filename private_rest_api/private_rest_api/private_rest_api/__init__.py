01d812616e42b0a9a0f64a66be2f5c7237b5d199
MatthewTee <teelucksingh.matthew1@gmail.com>
2022-02-06 02:44:03 -0500

    Added TODO and slight nginx config changes

--- private_rest_api/private_rest_api/private_rest_api/__init__.py
+++ private_rest_api/private_rest_api/private_rest_api/__init__.py
@@ -4,13 +4,5 @@ from .celery import app as celery_app
 __all__ = ("celery_app")
 
 # TODO: Address Secuirty issues with production deployment. 
-# TODO: Add project description for git repo.
-
-# TODO: Make the core url "/" display a map of the DRF schema via OpenAPI Schema. 
-# https://www.django-rest-framework.org/api-guide/schemas/
-# https://www.django-rest-framework.org/topics/documenting-your-api/#self-describing-apis
-
-# TODO: Create a markdown styled description of the reddit endpoint.
-# TODO: Correctly Style the main Swagger Homepage. https://drf-yasg.readthedocs.io/en/stable/index.html
-
+# TODO: Add project description for git repo README. Add Github project and wiki.
 # TODO: Create a scheduled database backup process.
