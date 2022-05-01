de0d8946e1a874c594f07c172c1804132a7f988f
MatthewTee <teelucksingh.matthew1@gmail.com>
2022-05-01 13:57:36 -0400

    fix import error

--- private_rest_api/private_rest_api/private_rest_api/cdn/conf.py
+++ private_rest_api/private_rest_api/private_rest_api/cdn/conf.py
@@ -1,3 +1,5 @@
+import os
+
 # Configuration for Digital Ocean staticfile storage:
 AWS_ACCESS_KEY_ID=os.environ.get("AWS_ACCESS_KEY_ID ")
 AWS_SECRET_ACCESS_KEY=os.environ.get("AWS_SECRET_ACCESS_KEY")
